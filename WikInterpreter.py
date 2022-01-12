512a7546bdcef3655cd090294346c06e3fe7a649
XenoKage <40901735+XenoKage@users.noreply.github.com>
2022-01-12 21:19:46 +0200

    WikInterpreter version 0.2.0
    See Release Notes in Releases page

--- WikInterpreter.py
+++ WikInterpreter.py
@@ -1,7 +1,10 @@
 import PySimpleGUI as sg
+import pyperclip
 
-magic_list=["[", "]"]
-nums = str([i for i in range(0,11)])
+magic_list = ["[", "]"]
+nums = str([i for i in range(0, 11)])
+
+
 
 def wikipedia_inter(strr):
     index_count = 0
@@ -37,54 +40,75 @@ def wikipedia_inter(strr):
 
     theret = "".join(toretl)
 
-    print(theret)
-    return(theret)
-
-
+    #print(theret)
+    return (theret)
 
 
 layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
 
-
 # All the stuff inside your window.
-layout = [  [sg.Text('Welcome to WikInterpreter!', justification='center')],
-            #[sg.Text('Enter your text:'),\
-            [sg.InputText()],
-            [sg.Button('Interpret!' , bind_return_key = True), sg.Button('Close')] ]
-
+layout = [[sg.Text('Welcome to WikInterpreter!', justification='center')],
+          [sg.InputText(key= 'ttinput' ,right_click_menu=['&Edit', ['Copy', 'Paste', 'Select All']])],
+          [sg.Button('Interpret!', bind_return_key=True), sg.Button('Close')]]
 
 # Create the Window
-window = sg.Window('WikInterpreter', layout, margins=(150,15), element_justification = 'c')
+window = sg.Window('WikInterpreter', layout, margins=(150, 15), element_justification='c', return_keyboard_events=True)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Close' or event == 'Interpret!': # if user closes window or clicks cancel
+    #print(event)
+    window['ttinput'].bind('<Control-v>', 'pest')
+    window['ttinput'].bind('<Control-a>', 'sel')
+    if event == sg.WIN_CLOSED or event == 'Close' or event == 'Interpret!':  # if user closes window or clicks cancel
+        values[0] = window['ttinput']
         break
-    
+    elif event == 'Copy':
+        pyperclip.copy(window['ttinput'].Widget.selection_get())
+    elif event == 'Paste' or event == 'ttinputpest':
+        try:
+            selection = (window['ttinput'].Widget.index("sel.first"), window['ttinput'].Widget.index("sel.last"))
+            if type(selection) != None:
+                window['ttinput'].Widget.delete(selection[0],selection[1])
+                #print(selection)
+        except:
+            pass
+        window['ttinput'].Widget.insert("insert",pyperclip.paste())
+    elif event == 'Select All' or event == 'ttinputsel':
+        window['ttinput'].Widget.select_range(0, 'end')
 
 window.close()
 
 
 
-wikival = wikipedia_inter(values[0])
+
+wikival = wikipedia_inter(values['ttinput'])
 pwikival = len(wikival) + 10
 
-if event == 'Interpret!' and values[0] != '':
+
+
+
+if event == 'Interpret!' and values['ttinput'] != '':
 
     layout = [[sg.Text("Text interpreted!", justification='center')], \
-              sg.InputText(wikival, use_readonly_for_disable=True, disabled=True, key='-IN-', justification='center' , \
-                           size = (pwikival,15))], \
-                            [sg.Button("OK" , bind_return_key = True)]
+              sg.InputText(wikival, use_readonly_for_disable=True, disabled=True, key='-IN-', justification='center', \
+                           size=(pwikival, 15), right_click_menu=['&Edit', ['Copy', 'Select All']])], \
+             [sg.Button("OK", bind_return_key=True)]
 
     # Create the window
-    window = sg.Window("WikInterpreter", layout, margins=(150,15), element_justification='c')
+    window = sg.Window("WikInterpreter", layout, margins=(150, 15), return_keyboard_events=True, \
+                       element_justification='c')
 
     # Create an event loop
     while True:
         event, values = window.read()
+        window['-IN-'].bind('<Control-a>', 'sel')
         # End program if user closes window or
         # presses the OK button
         if event == "OK" or event == sg.WIN_CLOSED:
             break
+        elif event == 'Copy':
+            pyperclip.copy(window['-IN-'].Widget.selection_get())
+        elif event == 'Select All' or event == '-IN-sel':
+            window['-IN-'].Widget.select_range(0, 'end')
 
-    window.close()
+window.close()
